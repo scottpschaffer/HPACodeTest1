c1d32a5e01b0c8740f3439944167d5cac31d58f8
Scott Schaffer <scottpschaffer@hotmail.com>
2017-04-30 19:34:04 -0500

    Took a wrong turn and am lost

--- HPACodeTest1/Form1.cs
+++ HPACodeTest1/Form1.cs
@@ -12,6 +12,10 @@ namespace HPACodeTest1
 {
     public partial class Form1 : Form
     {
+        List<List<int>> L0 = new List<List<int>>();
+        List<List<List<int>>> Alpha = new List<List<List<int>>>();
+        List<List<List<int>>> Beta = new List<List<List<int>>>();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,53 +23,72 @@ namespace HPACodeTest1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            L0.Add(new List<int> { 2, 2, 6 }); L0.Add(new List<int> { 2, 2, 6 }); L0.Add(new List<int> { 2, 2, 6 });
+            L0.Add(new List<int> { 2, 2, 6 }); L0.Add(new List<int> { 2, 2, 6 }); L0.Add(new List<int> { 2, 2, 6 });
+            L0.Add(new List<int> { 2, 2, 6 }); L0.Add(new List<int> { 2, 2, 6 }); L0.Add(new List<int> { 2, 2, 6 });
+            L0.Add(new List<int> { 2, 2, 6 }); L0.Add(new List<int> { 2, 6, 2 }); L0.Add(new List<int> { 2, 6, 2 });
+            Alpha.Add(L0);
         }
 
         public string Process(string input)
         {
             List<int> firstRow = new List<int>();
-            List<int> n = new List<int> { 2, 2, 6 };
-            List<List<int>> L0 = new List<List<int>>();
-            L0.Add(new List<int> { 2, 2, 6 });
-            L0.Add(new List<int> { 2, 6, 2 });
+            List<List<int>> allRows = new List<List<int>>();
+            List<bool> answer1 = new List<bool>();
+
             string[] testy1 = input.Split('\n');
-            for (var a = 0; a < testy1.Length; a++)
+            if (testy1.Length == 12)
             {
-                char[] split1 = testy1[a].ToCharArray();
-                firstRow = GetChars(split1);
-                bool qq = firstRow.SequenceEqual(L0[a]);
-                MessageBox.Show(qq.ToString());
+                for (var a = 0; a < testy1.Length; a++)
+                {
+                    char[] splity = testy1[a].ToCharArray();
+                    Beta.Add(GetChars(splity));
+                }
 
                 
+                for (var b1 = 0; b1 < 12; b1++)
+                {
+                    for (var b2 = 0; b2 < Beta[b1].Count; b2++)
+                    {
+                        answer1.Add(Beta[b1][b2].SequenceEqual(Alpha[b1][b2]));
+                    }
+                }
 
                 //MessageBox.Show(testy1[a]);
             }
-            return (firstRow[0].ToString() + "*" + firstRow[1].ToString() + "*" + firstRow[2].ToString()) ;
+            return (answer1.Contains(false).ToString()) ;
         }
 
         // Get arrangement of colons (non-letters) and spaces (parts that make up letters)
-        public List<int> GetChars(char[] lineOfChars)
+        public List<List<int>> GetChars(char[] lineOfChars)
         {
-            List<int> row = new List<int>();
-            int which1 = 0;
-            char c = ':';
-            int charNum = 0;
-            while (charNum < 10)
-            {
-                var count = 0;
+            List<List<int>> row0 = new List<List<int>>();
 
-                c = ((which1 % 2) == 0) ? ':' : ' ';
-                while ((charNum < 10) && (lineOfChars[charNum] == c))
+            if ((lineOfChars.Length % 12) == 0)
+            {
+                for (var aa = 0; aa < (lineOfChars.Length / 12); aa++)
                 {
-                    count++;
-                    charNum++;
+                    List<int> row1 = new List<int>();
+                    int which1 = 0;
+
+                    for (var bb = (aa * 12); bb < ((aa * 12) + 10); bb++)
+                    {
+                        var count = 0;
+                        var charNum = 0;
+                        var c = ((which1 % 2) == 0) ? ':' : ' ';
+                        if ((charNum < 10) && (lineOfChars[aa] == c))
+                        {
+                            count++;
+                            charNum++;
+                        }
+                        which1++;
+                        row1.Add(count);
+                    }
+                    row0.Add(row1);
                 }
-                which1++;
-                row.Add(count);
             }
 
-            return row;
+            return row0;
         }
 
         private void button1_Click(object sender, EventArgs e)
